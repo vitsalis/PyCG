c14b5e77eb4c43ab5807482cad3e08f37b9ea1a5
Vitalis Salis <vitsalis@gmail.com>
2019-12-02 14:04:41 +0200

    Delete test files that can be automatically generated

--- test_suite/builtins_test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from base import TestBase
-
-class BuiltinsTest(TestBase):
-    snippet_dir = "builtins"
-
-    def test_map(self):
-        self.validate_snippet(self.get_snippet_path("map"))
