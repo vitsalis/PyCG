0cb308cdc4161986d618f9701a60691e8ec9dc56
Vitalis Salis <vitsalis@gmail.com>
2020-03-25 15:16:02 +0200

    Add command line arguments for forge, product, version and timestamp

--- pycg/__main__.py
+++ pycg/__main__.py
@@ -10,25 +10,42 @@ def main():
     parser.add_argument("entry_point",
         nargs="*",
         help="Entry points to be processed")
-
     parser.add_argument(
         "--package",
         help="Package containing the code to be analyzed",
         default=None
     )
-
     parser.add_argument(
         "--try-complete",
         help="Try to produce a complete call graph",
         action="store_true",
         default=False
     )
-
     parser.add_argument(
         "--fasten",
         help="Produce call graph using the FASTEN format",
         action="store_true",
         default=False
+    )
+    parser.add_argument(
+        "--product",
+        help="Package name",
+        default=""
+    )
+    parser.add_argument(
+        "--forge",
+        help="Source the product was downloaded from",
+        default=""
+    )
+    parser.add_argument(
+        "--version",
+        help="Version of the product",
+        default=""
+    )
+    parser.add_argument(
+        "--timestamp",
+        help="Timestamp of the package's version",
+        default=0
     )
 
     args = parser.parse_args()
@@ -39,11 +56,11 @@ def main():
     output_cg = {}
 
     if args.fasten:
-        output_cg["product"] = ""
-        output_cg["forge"] = ""
+        output_cg["product"] = args.product
+        output_cg["forge"] = args.forge
         output_cg["depset"] = []
-        output_cg["version"] = ""
-        output_cg["timestamp"] = 0
+        output_cg["version"] = args.version
+        output_cg["timestamp"] = args.timestamp
         output_cg["cha"] = {}
         output_cg["graph"] = cg.output_edges()
     else:
