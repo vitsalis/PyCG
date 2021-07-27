vitsalis/PyCG

commit 45bf15cd1bb7a02f3e799fbd62f6881d43aed580

Added quiting mechanism in utils to prevent crash when analysing ipynwqb files.

pycg/utils/common.py
@@ -19,6 +19,7 @@
 # under the License.
 #
 import os
+import re
 
 def get_lambda_name(counter):
     return "<lambda{}>".format(counter)
@@ -37,3 +38,17 @@ def join_ns(*args):
 
 def to_mod_name(name, package=None):
     return os.path.splitext(name)[0].replace("/", ".")
+
+#This function quotes ipynb (notebook) specific symbols, such as "%" or "!"
+#TODO: Improve regexp for all possible crashing symbols
+def read_input_file(filename):
+    file_content = ""
+    with open(filename, "rt") as f:
+        for line in f:
+            if re.search(r'\s%|%', line): #searches line with the % symbol
+                line = "#" + line
+            if re.search(r'\s!|!', line): #searches line with the ! symbol
+
+                line = "#" + line
+            file_content = file_content + line
+    return file_content

pycg/processing/base.py
@@ -33,8 +33,7 @@ class ProcessingBase(ast.NodeVisitor):
 
         self.filename = os.path.abspath(filename)
 
-        with open(filename, "rt") as f:
-            self.contents = f.read()
+        self.contents = utils.read_input_file(self.filename)
 
         self.name_stack = []
         self.method_stack = []

commit cd485ac8e90f9469b24bc5336c0eab84c1282ff3

Fixed bug caused by previous 45bf15cd regexp commit.

pycg/utils/common.py
@@ -45,9 +45,9 @@ def read_input_file(filename):
     file_content = ""
     with open(filename, "rt") as f:
         for line in f:
-            if re.search(r'\s%|%', line): # searches line with the % symbol
+            if re.search(r'(^\s+\%)|(^\%)', line): # searches line with the % symbol
                 line = "#" + line
-            if re.search(r'\s!|!', line): # searches line with the ! symbol
+            if re.search(r'(^\s+\!)|(^\!)', line): # searches line with the ! symbol
                 line = "#" + line
             file_content = file_content + line
     return file_content