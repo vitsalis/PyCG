0a0cf0835b24e8061f9b697338e82e786a210e83
Ani Hovhannisyan <anihovhannisyang@gmail.com>
2021-08-03 16:22:57 +0900

    Refactored main. Added directory analysing logic using multiprocessing.

--- pycg/__main__.py
+++ pycg/__main__.py
@@ -5,9 +5,10 @@ import argparse
 
 from pycg.pycg import CallGraphGenerator
 from pycg import formats
-from pycg.utils.constants import CALL_GRAPH_OP, KEY_ERR_OP
+from pycg.utils.constants import CALL_GRAPH_OP, KEY_ERR_OP, LINENO_JSON
+from multiprocessing import Process
 
-def main():
+def parse_cmd_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("entry_point",
         nargs="*",
@@ -72,6 +73,11 @@ def main():
         default=False
     )
     parser.add_argument(
+        "--dir",
+        help="Directory path containing independent code to be analyzed",
+        default=None
+    )
+    parser.add_argument(
         "-o",
         "--output",
         help="Output path",
@@ -79,11 +85,9 @@ def main():
     )
 
     args = parser.parse_args()
+    return args
 
-    cg = CallGraphGenerator(args.entry_point, args.package,
-                        args.max_iter, args.operation)
-    cg.analyze()
-
+def prepare_output(args, cg):
     if args.operation == CALL_GRAPH_OP:
         if args.fasten:
             formatter = formats.Fasten(cg, args.package,
@@ -97,7 +101,9 @@ def main():
     if args.lineno:
         formatter_lineno = formats.LineNumber(cg)
         output_line_graph = formatter_lineno.generate()
-        print ("Line numbers graph is:\n", json.dumps(output_line_graph), "\n")
+        output_filename = args.entry_point[0] + LINENO_JSON
+        with open(output_filename, "w+") as f:
+            f.write(json.dumps(output_line_graph))
 
     as_formatter = formats.AsGraph(cg)
 
@@ -110,6 +116,43 @@ def main():
     if args.as_graph_output:
         with open(args.as_graph_output, "w+") as f:
             f.write(json.dumps(as_formatter.generate()))
+
+def run_analysing(args, filename = None):
+    if filename:
+        args.entry_point = filename
+        try:
+            cg = CallGraphGenerator(args.entry_point, args.package,
+                                args.max_iter, args.operation)
+            cg.analyze()
+        except Exception as e:
+            print("Something went wrong while analysing",
+                   args.entry_point, "file.\nError is:", e)
+    prepare_output(args, cg)
+
+# This function walks through directory and parallelly runs
+# separated threads for each independent file.
+def analyze_multiple_files(args):
+    fileslist = []
+    for (dirpath, dirnames, filenames) in os.walk(args.dir):
+        for filename in filenames:
+            fileslist.append(os.sep.join([dirpath, filename]))
+    # Following is the multiprocessing part to get call graph of each file
+    process = [None] * len(fileslist)
+    for i in range(len(fileslist)):
+        process[i] = Process(target=run_analysing, args=(args, [fileslist[i]]))
+        process[i].start()
+        process[i].join()
+
+def initiate_analyzing(args):
+    call_graphs = []
+    if args.dir:
+        analyze_multiple_files(args)
+    else:
+        run_analysing(args, args.entry_point)
+
+def main():
+    args = parse_cmd_args()
+    initiate_analyzing(args)
 
 if __name__ == "__main__":
     main()
