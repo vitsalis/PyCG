0c9884b7064bba1ecda64ec323b6ccaae4fec521
Vitalis Salis <vitsalis@gmail.com>
2022-03-24 21:50:30 +0200

    Bump up to version 0.0.5

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_long_desc():
 def setup_package():
     setup(
         name='pycg',
-        version='0.0.4',
+        version='0.0.5',
         description='Practical Python Call Graphs',
         long_description=get_long_desc(),
         long_description_content_type="text/markdown",
